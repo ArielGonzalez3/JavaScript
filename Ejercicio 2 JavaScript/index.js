735258e761c7918eb020de038032ba51754bee14
ArielGonzalez3 <100837478+ArielGonzalez3@users.noreply.github.com>
2022-11-12 21:17:04 +0100

    Create index.js

---
+++ Ejercicio 2 JavaScript/index.js
@@ -0,0 +1,15 @@
+// Ejercicio 2 JavaScript
+const array = [
+    "Elvis Ariel Gonzalez",
+    35,
+    true,
+    new Date("june 17 1987"),
+    {
+        titulo: "Harry Potter y el cáliz de fuego",
+        autor: "J.K. Rowling",
+        fecha: 2000,
+        url: "https://es.wikipedia.org/wiki/Harry_Potter_y_el_c%C3%A1liz_de_fuego"
+    }
+];
+
+console.log(lista);
