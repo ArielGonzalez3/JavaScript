68feade05cf21e5a43af98fb8261fef6756e8d6c
ArielGonzalez3 <100837478+ArielGonzalez3@users.noreply.github.com>
2022-11-15 22:08:05 +0100

    Create ejercicio-05-numbers.js

---
+++ Ejercici-05-numbers/ejercicio-05-numbers.js
@@ -0,0 +1,12 @@
+let altura_cm = 174;
+let altura_m = 1.74;
+let peso_kg = 52.4;
+
+let altura_m_redon = altura_m.toFixed();
+console.log(altura_m_redon);
+
+let peso_kg_redon = peso_kg.toPrecision(2);
+console.log(peso_kg_redon);
+
+let max_valor_js = Number.MAX_VALUE + 1;
+console.log(max_valor_js);
