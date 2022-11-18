7b1e09cef31c27792a2aab7819df68e151565604
ArielGonzalez3 <100837478+ArielGonzalez3@users.noreply.github.com>
2022-11-18 07:06:18 +0100

    Create conjuntos.js

---
+++ Ejercicio-07/conjuntos.js
@@ -0,0 +1,9 @@
+const familia = ["Ariel", "Sofía", "Alma", "Simon"];
+const familiaSet = new Set(familia);
+console.log(familiaSet);
+
+familiaSet.add("Ariel");
+console.log(familiaSet);
+
+familiaSet.add("JavaScript");
+console.log(familiaSet);
