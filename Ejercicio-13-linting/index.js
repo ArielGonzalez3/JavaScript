6b41eb57056758503817000511911f85f545e1bf
ArielGonzalez3 <100837478+ArielGonzalez3@users.noreply.github.com>
2022-11-26 22:59:14 +0100

    Create index.js

---
+++ Ejercicio-13-linting/index.js
@@ -0,0 +1,41 @@
+// Strings
+
+const strDbl = "Hola soy un texto con comillas dobles"
+const strSng = "Hola soy un texto con comillas simpres"
+
+console.log(strDbl)
+console.log(strSng)
+
+const strComillas = "El otro dia me dijo literalmente \"ve a sacar la Basura\""
+const strComillasSimples = "El otro dia me dijo literalmente \"ve a sacar la Basura\""
+const strComillasDobles = "El otro dia me dijo literalmente 've a sacar la Basura'"
+
+console.log(strComillas)
+console.log(strComillasSimples)
+console.log(strComillasDobles)
+
+/// /////////
+
+// comillas invertidas (backticks)
+const strBacktiks = "Hola esto es un string con backtiks"
+
+console.log(strBacktiks)
+
+const nombre = "Ariel"
+const saludo = `Hola, ${nombre} bienvenido`
+
+console.log(saludo)
+
+// Plantillas HTML
+
+const plantilla = `
+<html>
+    <h1>Página Web de ${nombre}</h1>
+    <p>Este es un parrafo</p>
+</html>
+`
+
+console.log(plantilla)
+
+/// //INtroduccion de variables en html
+const libros = ["empieza por el porque", "El monje que vendio su ferrari", "El poder del ahora"]
